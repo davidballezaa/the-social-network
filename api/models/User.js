e7cfbfcff4f57af313efa4843c43869e1dcc5597
davidballezaa <davidballezaa@gmail.com>
2022-08-06 16:19:29 -0500

    Add user model (mongoose schema)

---
+++ api/models/User.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      minLength: 3,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    description: {
+      type: String,
+      maxLength: 50,
+    },
+    city: {
+      type: String,
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
